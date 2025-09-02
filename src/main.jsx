7c6246eb57c23ebc87348642a79ca4305f0f412e
alexOrduL <aordunol95@gmail.com>
2025-09-01 20:21:17 -0600

    feat: main page usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './i18n/config' // Import i18n configuration
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n/config'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
   </StrictMode>,
 )
